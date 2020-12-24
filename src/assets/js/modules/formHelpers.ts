0beaf0e64169509722addf46c1158f88261af968
Ilya Belyavskiy <ilya.belyavskiy@swedbyte.se>
2020-12-25 02:19:49 +0300

    Form helpers refactoring

--- src/assets/js/modules/formHelpers.ts
+++ src/assets/js/modules/formHelpers.ts
@@ -4,35 +4,41 @@ export function validateField(fieldName: string, state: IFormInputState): [isVal
     const stateProp = state[fieldName];
     const [getFieldValue] = stateProp.value;
     const value = getFieldValue();
-    if ("inputField" in stateProp && "errorMessage" in stateProp) {
-        const {inputField, errorMessage} = stateProp;
-        if (inputField && errorMessage) {
-            if (isEmpty(value)) {
-                setError(inputField, errorMessage, "Cannot be empty", "form__input_error");
-                return [false, value];
-            }
-            if (typeof value === "string" && isXssPresent(value)) {
-                setError(inputField, errorMessage, "Invalid symbols", "form__input_error");
-                return [false, value];
-            }
-            if (stateProp.mustEqual !== undefined) {
-                const secondStateProp = state[stateProp.mustEqual];
-                const [getSecondFieldValue] = secondStateProp.value;
-                const secondFieldValue = getSecondFieldValue();
-                if (value !== secondFieldValue) {
-                    if ("inputField" in secondStateProp && "errorMessage" in secondStateProp) {
-                        const {inputField: secondInputField, errorMessage: secondErrorMessage} = secondStateProp;
-                        if (secondInputField && secondErrorMessage) {
-                            setError(inputField, errorMessage, "Values don't match", "form__input_error");
-                            setError(secondInputField, secondErrorMessage, "Values don't match", "form__input_error");
-                            return [false, value];
-                        }
-                    }
-                }
-            }
+
+    if (!("inputField" in stateProp)
+        || !("errorMessage" in stateProp)
+        || !stateProp.inputField
+        || !stateProp.errorMessage) {
+        return [true, value]
+    }
+
+    if (isEmpty(value)) {
+        setError(stateProp.inputField, stateProp.errorMessage, "Cannot be empty", "form__input_error");
+        return [false, value];
+    }
+    if (typeof value === "string" && isXssPresent(value)) {
+        setError(stateProp.inputField, stateProp.errorMessage, "Invalid symbols", "form__input_error");
+        return [false, value];
+    }
+    if (stateProp.mustEqual) {
+        const secondStateProp = state[stateProp.mustEqual];
+        const [getSecondFieldValue] = secondStateProp.value;
+        const secondFieldValue = getSecondFieldValue();
+
+        if (value !== secondFieldValue
+            && "inputField" in secondStateProp
+            && "errorMessage" in secondStateProp
+            && secondStateProp.inputField
+            && secondStateProp.errorMessage) {
+
+            setError(stateProp.inputField, stateProp.errorMessage, "Values don't match", "form__input_error");
+            setError(secondStateProp.inputField, secondStateProp.errorMessage, "Values don't match", "form__input_error");
+
+            return [false, value];
         }
     }
-    return [true, value];
+
+    return [true, value]
 }
 
 function setError(inputField: TObjectType, errorMessage: TObjectType, text: string, className: string): void {
@@ -67,43 +73,44 @@ function removeClass(inputField: TObjectType, className: string): string {
 export function setFormInput(event: Event, fieldName: string, state: IFormInputState, callback: (value: string) => unknown): void {
     const element = event.target as HTMLInputElement;
     const {value} = element;
-
     const stateProp = state[fieldName];
-    if ("inputField" in stateProp && "errorMessage" in stateProp) {
-        const {inputField, errorMessage} = stateProp;
-        if (inputField && errorMessage) {
-            const {attributes} = inputField.props;
-            const newClass = removeClass(inputField, "form__input_error");
-            inputField.setProps({
-                attributes: {
-                    ...attributes,
-                    class: newClass,
-                    value
-                }
-            })
-            setErrorMessage(errorMessage, "");
 
-            if (stateProp.mustEqual !== undefined) {
-                const secondStateProp = state[stateProp.mustEqual];
-                const [getSecondFieldValue] = secondStateProp.value;
-                const secondFieldValue = getSecondFieldValue();
-                if (value === secondFieldValue) {
-                    if ("inputField" in secondStateProp && "errorMessage" in secondStateProp) {
-                        const {inputField: secondInputField, errorMessage: secondErrorMessage} = secondStateProp;
-                        if (secondInputField && secondErrorMessage) {
-                            const newSecondClass = removeClass(secondInputField, "form__input_error");
-                            const secondFieldAttributes = secondInputField.props.attributes;
-                            secondInputField.setProps({
-                                attributes: {
-                                    ...secondFieldAttributes,
-                                    class: newSecondClass
-                                }
-                            });
-                            setErrorMessage(secondErrorMessage, "");
-                        }
-                    }
+    if (!("inputField" in stateProp)
+        || !("errorMessage" in stateProp)
+        || !stateProp.inputField
+        || !stateProp.errorMessage) {
+        return;
+    }
+
+    const {attributes} = stateProp.inputField.props;
+    const newClass = removeClass(stateProp.inputField, "form__input_error");
+    stateProp.inputField.setProps({
+        attributes: {
+            ...attributes,
+            class: newClass,
+            value
+        }
+    })
+    setErrorMessage(stateProp.errorMessage, "");
+
+    if (stateProp.mustEqual) {
+        const secondStateProp = state[stateProp.mustEqual];
+        const [getSecondFieldValue] = secondStateProp.value;
+        const secondFieldValue = getSecondFieldValue();
+        if (value === secondFieldValue
+            && "inputField" in secondStateProp
+            && "errorMessage" in secondStateProp
+            && secondStateProp.inputField
+            && secondStateProp.errorMessage) {
+            const newSecondClass = removeClass(secondStateProp.inputField, "form__input_error");
+            const secondFieldAttributes = secondStateProp.inputField.props.attributes;
+            secondStateProp.inputField.setProps({
+                attributes: {
+                    ...secondFieldAttributes,
+                    class: newSecondClass
                 }
-            }
+            });
+            setErrorMessage(secondStateProp.errorMessage, "");
         }
     }
 
