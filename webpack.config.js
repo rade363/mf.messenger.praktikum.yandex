0c2fdd7a2d72ba7b5573c3eaab40079a9ce55fa5
Ilya Belyavskiy <ilya.belyavskiy@swedbyte.se>
2021-01-15 00:04:20 +0300

    Webpack: TypeScript, SCSS, FileLoader (for images); + ESlint, Prettier, Husky (not enabled yet)

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: [
+        path.join(__dirname, "/src/assets/js/index.ts"),
+        path.join(__dirname, "/src/assets/scss/main.scss")
+    ],
+    output: {
+        path: path.resolve(__dirname, "./static/assets/js"),
+        filename: "index.js"
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    watchOptions: {
+        ignored: /node_modules/
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [
+                    {
+                        loader: "ts-loader",
+                        options: {
+                            configFile: path.resolve(__dirname, "tsconfig.json"),
+                        },
+                    }
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test:/\.(s*)css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                loader: "file-loader",
+                options: {
+                    name: "[name].[ext]",
+                    context: path.resolve(__dirname, "src/"),
+                    outputPath: "../img/",
+                    publicPath: "../",
+                    useRelativePaths: true
+                }
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "../css/style.css"
+        })
+    ]
+};
