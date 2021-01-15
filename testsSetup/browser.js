2488eddee0c14d7f726683fa753263504369b3e5
Ilya Belyavskiy <ilya.belyavskiy@swedbyte.se>
2021-01-15 20:03:16 +0300

    Fixed Handlebars testing with Mocha + Chai

--- testsSetup/browser.js
+++ testsSetup/browser.js
@@ -1,4 +1,6 @@
+const Handlebars = require("handlebars");
 const { JSDOM } = require("jsdom");
 const dom = new JSDOM("<!DOCTYPE html><html><head></head><body><div id='root'></div></body></html>", { url: "https://messenger.com/login/" });
 global["window"] = dom.window;
 global["document"] = dom.window.document;
+global["Handlebars"] = Handlebars;
